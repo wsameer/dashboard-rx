81363b65eda9fb8e5fc576d8379f695bf66e001a
wsameer <dev.wsameer@gmail.com>
2021-08-06 20:40:23 +0530

    style(SideNav): adds basic style to nav items

---
+++ src/components/layout/SideNav/SideNav.styled.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const SideNavContainer = styled('aside')`
+    display: flex;
+    flex-flow: column;
+    justify-content: start;
+    align-items: start;
+    width: 220px;
+    padding: 0 1rem;
+`;
+
+// export const FixedItems = styled('div')`
+//     margin-bottom: 1.6rem;
+// `;
+
+// export const CustomItems = styled('div')`
+//     border-top: 1px solid black;
+// `;
+
+export const SideNavList = styled('ul')`
+    padding: 0;
+    width: 100%;
+`;
+
+export const NavListItem = styled('li')`
+    align-items: center;
+    margin-bottom: 5px;
+    display: flex;
+    flex-grow: 1;
+    text-align: left;
+    border-radius: 12px;
+    padding: 12px 16px;
+
+    &:hover {
+        color: rgb(94, 53, 177);
+        background-color: rgb(237, 231, 246);
+    }
+`;
+
+export const NavListItemIcon = styled('div')`
+    display: inline-flex;
+    min-width: 36px;
+`;
+
+export const NavListItemText = styled('div')`
+    flex: 1 1 auto;
+    min-width: 0px;
+    margin-top: 4px;
+    margin-bottom: 4px;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.334em;
+`;
---
+++ src/components/layout/SideNav/SideNav.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Home, Trash2 } from 'react-feather';
+import * as Styled from './SideNav.styled';
+
+export const SideNav: React.FC = () => {
+    return (
+        <Styled.SideNavContainer>
+            <Styled.SideNavList>
+                <Styled.NavListItem>
+                    <Styled.NavListItemIcon>
+                        <Home size="20" />
+                    </Styled.NavListItemIcon>
+                    <Styled.NavListItemText>
+                        <span>Home</span>
+                    </Styled.NavListItemText>
+                </Styled.NavListItem>
+                <Styled.NavListItem>
+                    <Styled.NavListItemIcon>
+                        <Trash2 size="20" />
+                    </Styled.NavListItemIcon>
+                    <Styled.NavListItemText>
+                        <span>Trash</span>
+                    </Styled.NavListItemText>
+                </Styled.NavListItem>
+            </Styled.SideNavList>
+        </Styled.SideNavContainer>
+    );
+};
