e1c208bc747a6091ada425a73731537e4822a084
wsameer <dev.wsameer@gmail.com>
2021-08-08 20:50:55 +0530

    feat(Theme): adds Open Sans font

--- src/theme/fonts/fonts.ts
+++ src/theme/fonts/fonts.ts
@@ -7,14 +7,15 @@ import OpenSanSemiBold from './OpenSans/OpenSans-SemiBold.ttf';
 
 const FontStyles = createGlobalStyle`
     @font-face {
-    font-family: 'Open Sans';
-    src: url(${OpenSansBold}) format('truetype'),
-        url(${OpenSansBoldExtraBold}) format('truetype'),
-        url(${OpenSansLight}) format('truetype'),
-        url(${OpenSansRegular}) format('truetype'),
-        url(${OpenSanSemiBold}) format('truetype');
+        font-family: 'Open Sans';
+        src: local('Open Sans), 
+            url('${OpenSansLight}') format('truetype'),
+            url('${OpenSansRegular}') format('truetype'),
+            url('${OpenSanSemiBold}') format('truetype'),
+            url('${OpenSansBold}') format('truetype'),
+            url('${OpenSansBoldExtraBold}') format('truetype');
         font-style: normal;
-        font-weight: 300;
+        font-weight: normal;
     }
 `;
 
