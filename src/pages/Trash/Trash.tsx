8e8bfd797912825baf1687f70619c65ce3046579
wsameer <dev.wsameer@gmail.com>
2021-08-15 12:42:00 +0530

    feat(Trash): adds component to display all deleted items

---
+++ src/pages/Trash/Trash.tsx
@@ -0,0 +1,11 @@
+import Main from '@components/layout/Main';
+import React from 'react';
+
+export const Trash: React.FC = () => {
+    return (
+        <Main>
+            <h1>Hello Trash!</h1>
+        </Main>
+    );
+};
+Trash.displayName = 'Trash';
